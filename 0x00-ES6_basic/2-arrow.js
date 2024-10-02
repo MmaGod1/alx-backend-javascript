49df4f7c7bb8cb0ba83b73a1d9cfe98c7ab70896
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-10-02 08:16:57 +0100

    Update 2-arrow.js

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,6 +1,5 @@
 export default function getNeighborhoodsList() {
-  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
-
   this.addNeighborhood = (newNeighborhood) => {
-    self.sanFranciscoNeighborhoods.push(newNeighborhood);
-    return self.sanFranciscoNeighborhoods;
+  this.sanFranciscoNeighborhoods.push(newNeighborhood);
+  return this.sanFranciscoNeighborhoods;
+};
