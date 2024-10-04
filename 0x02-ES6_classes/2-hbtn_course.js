8cdfa5395c315cfe5d3656a7eb49d926437280df
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-10-04 14:55:00 +0100

    Lint style

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,47 +1,50 @@
 export default class HolbertonCourse {
-	constructor(name, length, students) {
-		if (typeof name !== 'string') {
-			throw new TypeError('name must be a string');
-		}
-		if (typeof length !== 'number') {
-			throw new TypeError('length must be a number');
-		}
-		if (!Array.isArray(students)) {
-			throw new TypeError('students must be an array');
-		}
+  constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new TypeError('name must be a string');
+    }
+    if (typeof length !== 'number') {
+      throw new TypeError('length must be a number');
+    }
+    if (!Array.isArray(students)) {
+      throw new TypeError('students must be an array');
+    }
 
-		this._name = name;
-		this._length = length;
-		this._students = students;
-	}
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
 
-	get name() {
-		return this._name;
-	}
+  get name() {
+    return this._name;
+  }
 
-	set name(value) {
-		if (typeof value !== 'string')
-			throw new TypeError('name must be a string');
-		this._name = value;
-	}
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('name must be a string');
+    }
+    this._name = value;
+  }
 
-	get length() {
-		return this._length;
-	}
+  get length() {
+    return this._length;
+  }
 
-	set length(len) {
-		if (typeof len !== 'number')
-			throw new TypeError('length must be a number');
-		this._length = len;
-	}
+  set length(len) {
+    if (typeof len !== 'number') {
+      throw new TypeError('length must be a number');
+    }
+    this._length = len;
+  }
 
-	get students() {
-		return this._students;
-	}
+  get students() {
+    return this._students;
+  }
 
-	set students(val) {
-		if (!Array.isArray(val))
-			throw new TypeError('students must be an array');
-		this._students = val;
-	}
-}
+  set students(val) {
+    if (!Array.isArray(val)) {
+      throw new TypeError('students must be an array');
+    }
+    this._students = val;
+  }
+	    }
