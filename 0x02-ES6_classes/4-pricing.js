4bfde9cad434d4f47d1557fb36e8f83594ad7964
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-10-04 15:42:01 +0100

    Create 4-pricing.js

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,31 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(val) {
+    this._amount = val;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+
+  set currency(value) {
+
+    this.currency = value;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+}
