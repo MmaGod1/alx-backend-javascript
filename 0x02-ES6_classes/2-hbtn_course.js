e1054a54f587097c52985a184d6d132355d5643e
MmaGod1 <gmmaduchukwu@gmail.com>
2024-10-04 13:19:15 +0000

    holberton

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,47 @@
+export default class HolbertonCourse {
+	constructor(name, length, students) {
+		if (typeof name !== 'string') {
+			throw new TypeError('name must be a string');
+		}
+		if (typeof length !== 'number') {
+			throw new TypeError('length must be a number');
+		}
+		if (!Array.isArray(students)) {
+			throw new TypeError('students must be an array');
+		}
+
+		this._name = name;
+		this._length = length;
+		this._student = student;
+	}
+
+	get name() {
+		return this._name;
+	}
+
+	set name(value) {
+		if (typeof name !== 'string')
+			throw new TypeError('name must be a string');
+		this._name = value;
+	}
+
+	get length() {
+		return this._length;
+	}
+
+	set length(len) {
+		if (typeof len !== 'number')
+			throw new TypeError('length must be a number');
+		this._length = len;
+	}
+
+	get students() {
+		return this._students;
+	}
+
+	set students(student) {
+		if (!Array.isArray(students))
+			throw new TypeError('students must be an array');
+		this._students = student;
+	}
+}
