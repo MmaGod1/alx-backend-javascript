bccb2d97a13e3baf3ec823f5cfce89c13193f49f
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-10-04 16:39:59 +0100

    Pause

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,10 @@
+export default Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+    if (this.constuctor !== Building && this.evacuationWarningMessage === undefined) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+  get sqft() {
+    return this._sqft;
+  }
