b6bc4c8d7bad7cc0cdc8601f3559c7f4568c1986
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-10-05 11:37:40 +0100

    Update 9-hoisting.js

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -20,7 +20,7 @@ export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
     this._lastName = lastName;
-    this._holbertonClass = holbertonClass;  // Fixed argument and assignment
+    this._holbertonClass = holbertonClass;
   }
 
   get fullName() {
@@ -28,11 +28,11 @@ export class StudentHolberton {
   }
 
   get holbertonClass() {
-    return this._holbertonClass;  // Fixed getter
+    return this._holbertonClass;
   }
 
   get fullStudentDescription() {
-    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;  // Fixed description and removed 'self'
+    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
   }
 }
 
