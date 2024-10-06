8f96d3de25b031c27507e4ce97a0983bf793fa12
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-10-06 09:59:15 +0100

    Update 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -9,6 +9,6 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
 
   return Promise.allSettled(promises).then((results) => results.map((result) => ({
     status: result.status,
-    value: result.status === 'fulfilled' ? result.value : result.reason,
+    value: result.status === 'fulfilled' ? result.value : `${result.reason}`,
   })));
 }
