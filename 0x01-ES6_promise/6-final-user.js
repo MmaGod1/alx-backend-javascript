0e27235a6c6f98109ffd991db5523169ec963d84
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-10-06 06:52:32 +0100

    Update 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,15 +1,14 @@
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  const signUpPromise = signUpUser(firstName, lastName);
-  const uploadPromise = uploadPhoto(fileName);
-
-  return Promise.allSettled([signUpPromise, uploadPromise])
-    .then(results => {
-      return results.map(result => ({
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+    const results = await Promise.allSettled([
+        signUpUser(firstName, lastName),
+        uploadPhoto(fileName)
+    ]);
+    
+    return results.map((result) => ({
         status: result.status,
-        value: result.status === 'fulfilled' ? result.value : result.reason,
-      }));
-    });
+        value: result.status === 'fulfilled' ? result.value : result.reason
+    }));
 }
