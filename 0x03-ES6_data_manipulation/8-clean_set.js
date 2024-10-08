c50a8ef87994da671057b1bc658401602378743a
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-10-08 17:23:19 +0100

    Update 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,4 @@
 export default function cleanSet(set, startString) {
   if (!startString) return '';
-  return [...set].filter((s)  => s.startsWith(startString)).map((s) => s.slice(startString.length)).join('-');
+  return [...set].filter((s) => s.startsWith(startString)).map((s) => s.slice(startString.length)).join('-');
 }
