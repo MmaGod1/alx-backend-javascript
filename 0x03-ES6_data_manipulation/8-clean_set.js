3b58d8a6e2a006ea90825b8e7af35cbdfce932ad
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-10-09 16:01:38 +0100

    Update 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,10 @@
-export default function cleanSet(set, startString) {
-  if (typeof startString !== 'string' || startString === '') return '';
-  return [...set]
-    .filter(value => value.startsWith(startString))
-    .map(value => value.slice(startString.length))
-    .join('-');
+startString) {
+  if (typeof startString !== 'string' || startString.length === 0) return '';
+  let result = '';
+  for (const value of set) {
+    if (value.startsWith(startString)) {
+      result += ${value.slice(startString.length)}-;
+    }
+  }
+  return result ? result.slice(0, -1) : '';
 }
